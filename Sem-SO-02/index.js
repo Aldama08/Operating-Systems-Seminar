cb37120de75a05e702e7a5ac9cb8b443d04c69a3
AldamaLAP <jose.aldama5891@alumno.udg.mx>
2024-08-21 13:54:38 -0600

    Ya empecé con el backend, hay unos detalles con el front. Enfocarse en el back

--- Sem-SO-02/index.js
+++ Sem-SO-02/index.js
@@ -1,5 +1,36 @@
-let Time, Name, ID, Operation, TME, TMF, Processes, MissingProcesses;
+// function processInputs() {
+//     // Constantes para cada elemento que quiero tomar
+//     // const addTime = document.getElementById('AddTime').value;
+//     // const name = document.getElementById('Name').value;
+//     // const id = document.getElementById('ID').value;
+//     // const operation = document.getElementById('Operation').value;
+//     // const tme = document.getElementById('TME').value;
 
-var a, b;
 
+//     // console.log('Time:', addTime);
+//     // console.log('Name:', name);
+//     // console.log('ID:', id);
+//     // console.log('Operation:', operation);
+//     // console.log('TME:', tme);
+// }
+
+// let i = 0;
+
+// function increment() {
+//     i++;
+//     console.log("Clic número: " + i);  // Muestra una alerta con el número de clics
+// }
+
+// // Vincula la función al botón después de que la página haya cargado
+// document.getElementById("Time").addEventListener('click', increment);
+
+function TextAreaOne()
+{
+    const TME = document.getElementById("Add-Time").value.trim(); //trim elimina espacios en blanco del contenido
+    const Name = document.getElementById("Name").value.trim(); 
+    const ID = document.getElementById("ID").value.trim(); 
+    const Op = document.getElementById("Operation").value.trim(); 
+    const TMR = document.getElementById("Name").value.trim(); 
+
+}
 
